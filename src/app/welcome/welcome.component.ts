0ef42811ca75ac49bbf50bee6cb9fad30a5e8ad8
Abhigyan Sinha <abhigyansinha2014@gmail.com>
2026-04-04 13:28:08 -0700

    [REFACTOR] - Welcome component

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
@@ -6,6 +6,24 @@ import { Component } from '@angular/core';
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit{
+  images = [
+    "/assets/teton.jpg",
+    "/assets/IMG_1102.jpeg",
+    "/assets/IMG_2105.jpeg",
+    "/assets/IMG_2274.jpeg",
+    "/assets/IMG_2311.jpeg",
+    "/assets/IMG_2362.jpeg"
+  ]
+  showOverlay = true;
+  slideDuration = 5;
 
+  ngOnInit() {
+    // Hide overlay after 3 seconds
+    setTimeout(() => (this.showOverlay = false), 3000);
+  }
+
+  getDelay(index: number): string {
+    return `${this.slideDuration * index}s`;
+  }
 }
