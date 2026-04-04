8918059bdb3857bf3ac12d4c0dc743446ed91eaa
Abhigyan Sinha <abhigyansinha2014@gmail.com>
2026-04-04 12:19:36 -0700

    [REFACTOR] : Style and design of the website

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,13 @@ import { Routes } from '@angular/router';
 import { GalleryComponent } from './gallery/gallery.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { RatesComponent } from './rates/rates.component';
-import { AvailabilityComponent } from './availability/availability.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     {path: '', component: WelcomeComponent},
     {path: 'gallery', component: GalleryComponent},
     {path: 'rates', component: RatesComponent},
-    {path: 'availability', component: AvailabilityComponent},
-    { path: 'testimonials', component: TestimonialsComponent }
+    {path: 'testimonials', component: TestimonialsComponent },
+    {path: 'contact', component: ContactComponent}
 ];
